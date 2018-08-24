74603ff9d7237f202aeb34a06adf4ec894e1dc3f
Dmitriy Rudenskiy <dmitriy.rudenskiy@gmail.com>
2018-08-24 15:31:41 +0700

    add: send form

--- public/js/main.js
+++ public/js/main.js
@@ -1,102 +1,173 @@
 /* ======= Preloader ======= */
 
 /* ======= Testinomial Slider ======= */
- $('.slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.slider-nav'
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
 });
 
 $('.slider-nav').slick({
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  asNavFor: '.slider-for',
-  dots: false,
-  centerMode: true,
-  focusOnSelect: true,
-  responsive: [
-    {
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      }
-    }, 
-  ]
-}).on('beforeChange', function(event, slick, currentSlide, nextSlide){
-  RemoveClass();
-  AddClass();
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+    ]
+}).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    RemoveClass();
+    AddClass();
 });
 
-$(".slick-prev").click(function(){
-	RemoveClass();
-	AddClass();
+$(".slick-prev").click(function () {
+    RemoveClass();
+    AddClass();
 });
 
-$(".slick-next").click(function(){
-	RemoveClass();
-	AddClass();
+$(".slick-next").click(function () {
+    RemoveClass();
+    AddClass();
 });
 
 // On edge hit
-$('.slider-nav').on('edge', function(event, slick, direction){
-  	RemoveClass();
-	AddClass();
+$('.slider-nav').on('edge', function (event, slick, direction) {
+    RemoveClass();
+    AddClass();
 });
 
-// On swipe event	
-$('.slider-nav').on('swipe', function(event, slick, direction){
-	RemoveClass();
-	AddClass();
+// On swipe event
+$('.slider-nav').on('swipe', function (event, slick, direction) {
+    RemoveClass();
+    AddClass();
 });
 
-$('.slider-nav').click(function(e) {
-  RemoveClass();
-	AddClass();
+$('.slider-nav').click(function (e) {
+    RemoveClass();
+    AddClass();
 });
 
 
-$( document ).ready(function() {	
-	AddClass();
+$(document).ready(function () {
+    AddClass();
 });
 
 function RemoveClass() {
-	$('div.slick-slide').removeClass('first');
-	$('div.slick-slide').removeClass('second');
-	$('div.slick-slide').removeClass('third');
-	$('div.slick-slide').removeClass('fourth');
-	$('div.slick-slide').removeClass('fifth');
-	$('div.slick-slide').removeClass('sixth');
-	$('div.slick-slide').removeClass('seven');
+    $('div.slick-slide').removeClass('first');
+    $('div.slick-slide').removeClass('second');
+    $('div.slick-slide').removeClass('third');
+    $('div.slick-slide').removeClass('fourth');
+    $('div.slick-slide').removeClass('fifth');
+    $('div.slick-slide').removeClass('sixth');
+    $('div.slick-slide').removeClass('seven');
 }
 
 function AddClass() {
-	$('.slick-track').find('.slick-center').addClass('third');
-	$('.slick-center').prev("div.slick-active:first").addClass("first");
-	$('.slick-center').prev("div.slick-active:first").prev("div.slick-active").addClass("second");
-	$('.second').prev("div.slick-slide").addClass("sixth");
-	$('.slick-center').next("div.slick-active:first").addClass("fourth");
-	$('.slick-center').next("div.slick-active:first").next("div.slick-active").addClass("fifth");
-	$('.fifth').next("div.slick-slide").addClass("seven");
+    $('.slick-track').find('.slick-center').addClass('third');
+    $('.slick-center').prev("div.slick-active:first").addClass("first");
+    $('.slick-center').prev("div.slick-active:first").prev("div.slick-active").addClass("second");
+    $('.second').prev("div.slick-slide").addClass("sixth");
+    $('.slick-center').next("div.slick-active:first").addClass("fourth");
+    $('.slick-center').next("div.slick-active:first").next("div.slick-active").addClass("fifth");
+    $('.fifth').next("div.slick-slide").addClass("seven");
 }
-$(document).ready(function($) {
-	$(window).on('load',function(){
-		$('#preloader').fadeOut('slow',function(){$(this).remove();});
-	});
-	/* ======= Scrolllt ======= */
-	$.scrollIt({
-		topOffset: -95, // offste (in px) for fixed top navigation        
-	});
 
-	/* ======= Accordian ======= */
-	function toggleIcon(e) {
-		$(e.target)
-			.prev('.panel-heading')
-			.find(".more-less")
-			.toggleClass('glyphicon-plus glyphicon-minus');
-	}
-	$('.panel-group').on('hidden.bs.collapse', toggleIcon);
-	$('.panel-group').on('shown.bs.collapse', toggleIcon);
+$(document).ready(function ($) {
+    $(window).on('load', function () {
+        $('#preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    });
+    /* ======= Scrolllt ======= */
+    $.scrollIt({
+        topOffset: -95, // offste (in px) for fixed top navigation
+    });
+
+    /* ======= Accordian ======= */
+    function toggleIcon(e) {
+        $(e.target)
+            .prev('.panel-heading')
+            .find(".more-less")
+            .toggleClass('glyphicon-plus glyphicon-minus');
+    }
+
+    $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+    $('.panel-group').on('shown.bs.collapse', toggleIcon);
 });
+
+// Send mail
+$(document).ready(function () {
+    $('#get_phone form').submit(function () {
+
+        $('<div>').addClass('text-success').html('<h2>Наш менеджер уже набирает ваш телефон</h2>').insertAfter($(this).parent());
+        $(this).parent().hide();
+
+        $.ajax({
+            url: "/mail/send",
+            type: "POST",
+            data: $(this).serialize(),
+            success: function (data) {
+                console.log(data);
+            }
+        });
+
+        return false;
+    });
+
+    $('.__top_big_form_wrap form').submit(function () {
+
+        $('<div>').addClass('text-success').html('<h2 class="text-center">Наш менеджер уже набирает ваш телефон</h2>').insertAfter($(this).parent());
+        $(this).parent().hide();
+
+        $.ajax({
+            url: "/mail/form",
+            type: "POST",
+            data: $(this).serialize(),
+            success: function (data) {
+                console.log(data);
+            }
+        });
+
+        return false;
+    });
+});
+
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function () {
+        try {
+            w.yaCounter43318404 = new Ya.Metrika({
+                id: 43318404,
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true
+            });
+        } catch (e) {
+        }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () {
+            n.parentNode.insertBefore(s, n);
+        };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else {
+        f();
+    }
+})(document, window, "yandex_metrika_callbacks");
--- src/http/controllers/mail.controller.js
+++ src/http/controllers/mail.controller.js
@@ -1,75 +1,62 @@
-import fs from 'fs'
-import path from 'path'
 import Mailer from '../../services/mailgun.service'
 
 const MailController = {}
 module.exports = MailController
 
 MailController.send = async (ctx, next) => {
-    let sourceId = (ctx.request.body.source_id || -1) * 1
-    let email = ctx.request.body.email || 'нет'
-    let subject = ctx.request.body.subject || 'нет'
-    let name = ctx.request.body.name || 'нет'
-    let phone = ctx.request.body.phone || '0'
-    let vin = ctx.request.body.vin || 'нет'
-    let comment = ctx.request.body.comment || 'нет'
-    const now = new Date()
 
-    if (email) {
-        email = email.trim().toLowerCase()
-    }
+    console.log(ctx.request.body);
 
-    if (subject) {
-        subject = subject.trim()
-    }
-
-    name = name
+    const refer = ctx.request.get('Referrer') || ctx.headers.referrer || ctx.headers.referer
+    const name = ctx.request.body.name
         .toString()
+        .substr(0, 255)
         .trim()
-        .substring(0, 50)
+    const phone = ctx.request.body.phone.toString().replace(/[^0-9]/g, '')
 
-    phone = '+7' + phone.replace(/[^0-9]/gi, '').slice(-10)
+    const message =
+        'Сайт: ' +
+        refer +
+        '\n\tЗаявка на просчёт\n\tИмя: ' +
+        name +
+        '\n\tТелефон: ' +
+        phone
 
-    vin.toString().substring(0, 25)
+    Mailer(process.env.MAILGUN_ADMIN, 'Заявка с сайта', message)
 
-    comment
-        .toString()
-        .trim()
-        .substring(0, 255)
-
-    const message = await ctx.render('mail/template', {
-        name,
-        phone,
-        vin,
-        comment,
-        sourceId,
-        email,
-    })
-
-    Mailer(
-        process.env.MAILGUN_ADMIN,
-        'Тестовое сообщение для проверки',
-        null,
-        message
-    )
-    // ctx.body = message
-
-    fs.appendFile(
-        path.join(__dirname, '/../../../public') + '/log_phones.txt',
-        [
-            [now.getDate(), now.getMonth() + 1, now.getFullYear()].join('.'),
-            sourceId,
-            phone,
-            name,
-            vin,
-            comment,
-        ].join(' | ') + '\n',
-        error => {}
-    )
-
-    ctx.redirect(ctx.router.url('home', { query: { success: 1 } }))
+    ctx.body = 'Ok';
 }
 
 MailController.form = async (ctx, next) => {
-    ctx.body = await ctx.render('home/contact', { queries, page, pages })
+    const refer =
+        ctx.get('Referrer') || ctx.headers.referrer || ctx.headers.referer
+    const phone = ctx.request.body.phone.toString().replace(/[^0-9]/g, '')
+
+    // Данные из формы
+    const sparePart = ctx.request.body.spare_part || '>>>НЕ УКАЗАНО<<<'
+    const city = ctx.request.body.city || '>>>НЕ УКАЗАНО<<<'
+    const maker = ctx.request.body.maker || '>>>НЕ УКАЗАНО<<<'
+    const model = ctx.request.body.model || '>>>НЕ УКАЗАНО<<<'
+    const vin = ctx.request.body.vin || '>>>НЕ УКАЗАНО<<<'
+    const year = ctx.request.body.year || '>>>НЕ УКАЗАНО<<<'
+
+    // Text
+    let text = '\tНазвание запчасти: ' + sparePart + '\n'
+    text += '\tВ городе: ' + city + '\n'
+    text += '\tМарка: ' + maker + '\n'
+    text += '\tМодель: ' + model + '\n'
+    text += '\tVin-code или Frame: ' + vin + '\n'
+    text += '\tГод выпуска: ' + year + '\n'
+
+    const message =
+        'Сайт: ' +
+        refer +
+        '\n\tЗаявка c верхней формы\n\tСодержание: \n\n' +
+        text +
+        '\n\tТелефон: ' +
+        phone
+
+    Mailer(process.env.MAILGUN_ADMIN, '[Новая] Заявка с сайта', message)
+
+    ctx.body = 'Ok';
 }
