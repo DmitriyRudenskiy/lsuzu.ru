890389921a0936be74da84898989af3d5177161d
Dmitriy Rudenskiy <dmitriy.rudenskiy@gmail.com>
2018-08-24 13:47:40 +0700

    add: static page

--- src/http/controllers/home.controller.js
+++ src/http/controllers/home.controller.js
@@ -2,14 +2,9 @@ const HomeController = {}
 module.exports = HomeController
 
 HomeController.about = async (ctx, next) => {
-    ctx.body = await ctx.render('home/about', { queries, page, pages })
+    ctx.body = await ctx.render('home/about', {})
 }
 
 HomeController.contact = async (ctx, next) => {
-    ctx.body = await ctx.render('home/contact', {
-        title: 'Список',
-        queries,
-        page,
-        pages,
-    })
+    ctx.body = await ctx.render('home/contact', {})
 }
